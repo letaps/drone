3ebf4501e56a8dfae02ec1c0097320b8bbae6402
tchibindi <tapschibz@gmail.com>
2022-08-25 15:52:57 +0200

    :tada: Initial Commit

---
+++ src/main/java/com/musala/drones/Enums/model.java
@@ -0,0 +1,11 @@
+package com.musala.drones.Enums;/*
+ *@CREATED    :  25-08-2022 : 15:49
+ *@PROJECT    :  drones
+ *@AUTHOR     :  TAPIWA CHIBINDI
+ *EMAIL       :  tapschibz@gmail.com
+
+ */
+
+public enum model {
+    Lightweight, Middleweight, Cruiserweight, Heavyweight
+}
---
+++ src/main/java/com/musala/drones/Enums/state.java
@@ -0,0 +1,11 @@
+package com.musala.drones.Enums;/*
+ *@CREATED    :  25-08-2022 : 15:39
+ *@PROJECT    :  drones
+ *@AUTHOR     :  TAPIWA CHIBINDI
+ *EMAIL       :  tapschibz@gmail.com
+
+ */
+
+public enum state {
+    IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING
+}
